16e73f4aa49ce2eb65214dcc95423af13e61f1f6
Emilse Arias <emilsearias@gmail.com>
2025-10-02 20:56:39 -0300

    agregamos la clase Server con la configuracion del puerto y lo middlewares

---
+++ index.js
@@ -0,0 +1,7 @@
+import Server from "./src/server/config.js";
+
+const server = new Server();
+
+//agregar las rutas
+
+server.listen()
---
+++ src/server/config.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+//1- Tomar un puerto
+//2- configura los middlewares
+//3- usa las rutas
+export default class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3001;
+    this.middlewares();
+  }
+
+  middlewares() {
+    this.app.use(cors()); //permite conexiones remotas
+    this.app.use(express.json()); //permite interpretar los datos que lleguen en la solicitud en formato json
+    this.app.use(morgan("dev")); // nos ofrece datos extras en la terminal
+    //configurar un archivo estatico
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    console.log(__dirname);
+    console.log(__dirname + "/public");
+    this.app.use(express.static(__dirname + "/public"));
+  }
+
+  listen() {
+    this.app.listen(this.port, () =>
+      console.info("El servidor se esta ejecutando en el puerto:" + this.port)
+    );
+  }
+}
