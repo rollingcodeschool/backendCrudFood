0c0b559724bab0ccf96ce000583bd4782d66e78b
Emilse Arias <emilsearias@gmail.com>
2025-10-07 20:56:33 -0300

    agregamos la ruta y controlador para crear productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -3,3 +3,7 @@ export const prueba = (req, res)=>{
  res.send('Prueba desde el controlador')
 }
 
+export const crearProducto = (req, res)=>{
+ res.send('Aqui tenemos que crear el producto')
+}
+
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { prueba } from "../controllers/productos.controllers.js";
+import { crearProducto, prueba } from "../controllers/productos.controllers.js";
 /*
 GET
 POST
@@ -10,5 +10,6 @@ DELETE
 const router = Router()
 
 router.route('/test').get(prueba)
+router.route('/').post(crearProducto)
 
 export default router
