22d034cb603688997f0e73258dcbce7bd202a446
Emilse Arias <emilsearias@gmail.com>
2025-10-09 21:35:36 -0300

    agregamos la logica para crearProducto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,10 +1,20 @@
-export const prueba = (req, res)=>{
- console.log('desde el controlador de prueba')
- res.send('Prueba desde el controlador')
-}
+import Producto from "../models/producto.js";
 
-export const crearProducto = (req, res)=>{
- res.send('Aqui tenemos que crear el producto')
+export const prueba = (req, res) => {
+  console.log("desde el controlador de prueba");
+  res.send("Prueba desde el controlador");
+};
 
-}
-
+export const crearProducto = async (req, res) => {
+  try {
+    //1- verificar que llegan los datos validados
+    //2- pedir al modelo Producto crear el objeto en la base de datos
+    console.log(req.body)
+    const productoNuevo = new Producto(req.body)
+    await productoNuevo.save()
+    res.status(201).json({ mensaje: 'El producto fue creado exitosamente'})
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Ocurrio un error al crear el producto" });
+  }
+};
