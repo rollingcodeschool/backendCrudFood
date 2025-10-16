4e985464e684d3e3ffacddf8e868190a16c450db
Emilse Arias <emilsearias@gmail.com>
2025-10-16 19:56:24 -0300

    ajustamos el controlador de editar y borrar

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -46,12 +46,10 @@ export const obtenerProducto = async (req, res) => {
 
 export const borrarProductoPorID = async (req, res) => {
   try {
-  console.log(req.params.id)
-  const productoBuscado = await Producto.findById(req.params.id)
+  const productoBuscado = await Producto.findByIdAndDelete(req.params.id)
   if(!productoBuscado){
     return res.status(404).json({ mensaje: "No se encontro el producto" })
   }
-  await Producto.findByIdAndDelete(req.params.id)
   res.status(200).json({mensaje:'El producto fue eliminado correctamente'})
   } catch (error) {
     console.error(error);
@@ -61,11 +59,10 @@ export const borrarProductoPorID = async (req, res) => {
 
 export const editarProductoPorID = async (req, res) => {
   try {
-  const productoBuscado = await Producto.findById(req.params.id)
+  const productoBuscado = await Producto.findByIdAndUpdate(req.params.id, req.body)
   if(!productoBuscado){
     return res.status(404).json({ mensaje: "No se encontro el producto" })
   }
-  await Producto.findByIdAndUpdate(req.params.id, req.body)
   res.status(200).json({mensaje:'El producto fue editado correctamente'})
   } catch (error) {
     console.error(error);
