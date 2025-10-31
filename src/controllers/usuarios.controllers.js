dc745d0876e4e2a4eeaa7e9f147d6bb1abdfa3ec
Emilse Arias <emilsearias@gmail.com>
2025-10-30 21:39:25 -0300

    agregamos la funcion de login de usuarios

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -28,3 +28,34 @@ export const listarUsuarios = async (req, res) => {
       .json({ mensaje: "Ocurrio un error, no se pudo listar los usuarios" });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //verificar el email
+    // const usuarioBuscado = await Usuario.findOne({email: req.body.email})
+    const usuarioBuscado = await Usuario.findOne({ email });
+    if (!usuarioBuscado) {
+      return res.status(404).json({ mensaje: "El usuario no existe" });
+    }
+    //chequear el password
+    const passwordValido = bcrypt.compareSync(
+      password,
+      usuarioBuscado.password
+    );
+    if (!passwordValido) {
+      return res.status(401).json({ mensaje: "Contraseña incorrecta" });
+    }
+    res
+      .status(200)
+      .json({
+        mensaje: "Usuario logueado correctamente",
+        usuario: usuarioBuscado.nombreUsuario,
+      });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "ocurrio un error al intentar loguear un usuario" });
+  }
+};
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { crearUsuario, listarUsuarios } from "../controllers/usuarios.controllers.js";
+import { crearUsuario, listarUsuarios, login } from "../controllers/usuarios.controllers.js";
 
 const router = Router();
 
 router.route("/").post(crearUsuario).get(listarUsuarios);
+router.route('/login').post(login)
 
 
 export default router;
