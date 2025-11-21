61ed0ff51c085487b248bf695ea2bd75ff87bb2c
Emilse Arias <emilsearias@gmail.com>
2025-11-20 21:37:54 -0300

    agregamos un controlador para armar una preferencia de pago de mercadoPago, adicionamos un webhook para actualizar el estado de mi pedido

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import productosRoutes from './productos.routes.js'
 import usuariosRouter from "./usuarios.routes.js";
+import pagosRouter from "./pagos.routes.js";
+
 
 const router = Router()
 // http://localhost:3000/api/productos/
 router.use('/productos', productosRoutes)
 router.use('/usuarios', usuariosRouter)
+router.use('/pagos', pagosRouter)
 
 export default router
